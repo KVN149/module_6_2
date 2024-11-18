9efb3fef6c1b87337ececa2a06821065b8484978
KVN149 <idooll@bk.ru>
2024-11-18 19:55:21 +0400

    Add files via upload

--- module_6_2.py
+++ module_6_2.py
@@ -1,26 +1,26 @@
 class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner, __model, __color, __engine_power):
+    def __init__(self, owner, model, color, engine_power):
         self.owner = owner
-        self.__model = __model
-        self.__color = __color
-        self.__engine_power = __engine_power
+        self.__model = model
+        self.__color = color
+        self.__engine_power = engine_power
 
     def set_color(self, new_color):
         if new_color.lower() in (color.lower() for color in self.__COLOR_VARIANTS):
-            self.__color = str(new_color)
+            self.__color = new_color
         else:
             print(f'Нельзя сменить цвет на {new_color}')
 
-    def get_model(self, __model):
-        self.__model = __model
+    def get_model(self):
+        return self.__model
 
-    def get_horsepower(self, __engine_power):
-        self.__engine_power = __engine_power
+    def get_horsepower(self):
+        return self.__engine_power
 
-    def get_color(self, __color):
-        self.__color = __color
+    def get_color(self):
+        return self.__color
 
     def print_info(self):
         print(f'Модель: {self.__model}')
